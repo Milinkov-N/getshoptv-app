5d3cb0c91f6976dad69c9e9ff78cdf0e07209366
Milinkov-N <milinkov.nik@gmail.com>
2022-01-27 22:55:48 +0400

    Finished Keyboard component

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,7 +1,64 @@
 import './keyboard.css'
 
 export default function Keyboard() {
+  const keys = [
+    {
+      id: 'key-1',
+      value: '1',
+    },
+    {
+      id: 'key-2',
+      value: '2',
+    },
+    {
+      id: 'key-3',
+      value: '3',
+    },
+    {
+      id: 'key-4',
+      value: '4',
+    },
+    {
+      id: 'key-5',
+      value: '5',
+    },
+    {
+      id: 'key-6',
+      value: '6',
+    },
+    {
+      id: 'key-7',
+      value: '7',
+    },
+    {
+      id: 'key-8',
+      value: '8',
+    },
+    {
+      id: 'key-9',
+      value: '9',
+    },
+    {
+      id: 'erase',
+      value: 'Стереть',
+    },
+    {
+      id: 'key-0',
+      value: '0',
+    },
+  ]
+
   return (
-    <div>keyboard</div>
+    <div className="keyboard">
+      {keys.map(key => (
+        <button
+          key={ key.id }
+          className={ `keyboard__key ${ key.id === 'erase' ? 'col-span-2' : '' }` }
+          id={ key.id }
+        >
+          { key.value }
+        </button>
+      ))}
+    </div>
   )
 }
--- src/components/Promo.js
+++ src/components/Promo.js
@@ -1,4 +1,5 @@
 import './promo.css'
+import Keyboard from './Keyboard'
 
 export default function Promo() {
   return (
@@ -9,6 +10,11 @@ export default function Promo() {
         </h2>
         <div className="panels__number">+7(___)___-__-__</div>
         <span className="panels__desc">и с Вами свяжется наш менеждер для дальнейшей консультации</span>
+        <Keyboard />
+        <div>
+          <input type='checkbox' name='checkbox' id='checkbox' />
+          <label for='checkbox'>Согласие на обработку персональных данных</label>
+        </div>
       </div>
     </div>
   )
