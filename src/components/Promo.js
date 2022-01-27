9d39589d530b2a088c9eb80abefce6942c7814ac
Milinkov-N <milinkov.nik@gmail.com>
2022-01-27 23:17:07 +0400

    Added checkbox and confirm button in Promo component

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -5,16 +5,19 @@ export default function Promo() {
   return (
     <div className="promo__wrapper">
       <div className="promo__panels">
-        <h2 className="panels__header">
-          Введите ваш номер мобильного телефона
-        </h2>
-        <div className="panels__number">+7(___)___-__-__</div>
-        <span className="panels__desc">и с Вами свяжется наш менеждер для дальнейшей консультации</span>
-        <Keyboard />
-        <div>
-          <input type='checkbox' name='checkbox' id='checkbox' />
-          <label for='checkbox'>Согласие на обработку персональных данных</label>
-        </div>
+        <div className="panels__wrapper">
+          <h2 className="panels__header">
+            Введите ваш номер мобильного телефона
+          </h2>
+          <div className="panels__number">+7(___)___-__-__</div>
+          <span className="panels__desc">и с Вами свяжется наш менеждер для дальнейшей консультации</span>
+          <Keyboard />
+          <div className="panels__agreement">
+            <input type='checkbox' name='checkbox' id='checkbox' />
+            <label htmlFor='checkbox'>Согласие на обработку персональных данных</label>
+          </div>
+          <button className="panels__btn" disabled>Подтвердить номер</button>
+        </div> 
       </div>
     </div>
   )
