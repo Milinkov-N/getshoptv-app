0ef81a1aaba7a58511d0099505323e0f2f90b151
Milinkov-N <milinkov.nik@gmail.com>
2022-01-31 18:59:27 +0400

    changed api url in validateNumber.js to relative

--- src/utils/validateNumber.js
+++ src/utils/validateNumber.js
@@ -2,9 +2,11 @@ export default async function validateNumber(number) {
   const numString = number.join('')
 
   try {
-    const URL = `http://apilayer.net/api/validate?access_key=${ process.env.REACT_APP_NUMERIFY_ACCESS_KEY }&number=${ numString }&country_code=RU&format=1`
+    const URL = `//apilayer.net/api/validate?access_key=${ process.env.REACT_APP_NUMERIFY_ACCESS_KEY }&number=${ numString }&country_code=RU&format=1`
     const data = await fetch(URL)
     const res = await data.json()
+
+    console.log(res);
 
     return res
   } catch(e) {
